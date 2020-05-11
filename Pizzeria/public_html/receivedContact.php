c7d309a1f50167b6054b40f13d996dcc755cf037
Guillaume Lissot <46529878+Memphis-Bell@users.noreply.github.com>
2020-05-11 17:28:49 +0200

    final

---
+++ Pizzeria/public_html/receivedContact.php
@@ -0,0 +1,8 @@
+<?php
+echo 'Quelqu\'un vous à contacté : <br>';
+echo 'Nom : '. $_POST["nom"].'<br>';
+echo 'Prenom : '.$_POST["prenom"].'<br>';
+echo 'Email : '.$_POST["email"].'<br>';
+echo 'Telephone : '.$_POST["tel"].'<br>';
+echo 'Canal de contact : '.$_POST["channel"].'<br>';
+echo 'Message : '.$_POST["message"].'<br>';
